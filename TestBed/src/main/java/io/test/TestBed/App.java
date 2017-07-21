3b575e8b45c072c052bc6266e59c77c7d4e0a2c3
Tyrillius <tyrillius@gmail.com>
2017-07-21 13:50:12 -0400

    Test again

--- TestBed/src/main/java/io/test/TestBed/App.java
+++ TestBed/src/main/java/io/test/TestBed/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!! TESTING!!! AGAIN!!!" );
+        System.out.println( "Hello World!!!! TESTING!!! AGAIN!!! LAST TIME!!!" );
     }
 }
